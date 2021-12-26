63bad2d0ac82288aea9ee0423cca94b67418b234
SuilandCoder <songjie0613@126.com>
2021-12-26 17:49:45 +0800

    practice of minigrep

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,23 +1,89 @@
-use std::fs;
 use std::error::Error;
+use std::{env, fs};
 
-pub struct Config{
-    query:String,
-    filename:String
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+    pub case_sensitive: bool,
 }
 
-impl Config{
-    pub fn new(args:&[String])->Result<Config,&'static str>{
-        if args.len()<3{
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
             return Err("not enough arguments");
         }
         let query = args[1].clone();
-        let filename=args[2].clone();
-        Ok(Config{query,filename})
+        let filename = args[2].clone();
+
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
     }
 }
 
-pub fn run(config:Config)->Result<(),Box<dyn Error>>{
-    let contents = fs::read_to_string(config.filename)?; 
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insentitive(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{}", line);
+    }
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut res = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            res.push(line);
+        }
+    }
+    res
+}
+
+pub fn search_case_insentitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut res = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            res.push(line);
+        }
+    }
+    res
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insentitive() {
+        let query = "rUst";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insentitive(query, contents)
+        );
+    }
 }
